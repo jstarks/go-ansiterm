5c64c1b361997da61146ad1aca6b2f0418a05ee5
John Starks <jostarks@microsoft.com>
2015-07-14 15:33:28 -0700

    Add support for old C1 commands: IND, NEL
    Signed-off-by: John Starks <jostarks@microsoft.com>

--- parser_actions.go
+++ parser_actions.go
@@ -25,7 +25,15 @@ func (ap *AnsiParser) escDispatch() error {
 	logger.Infof("escDispatch: %v(%v)", cmd, intermeds)
 
 	switch cmd {
-	case "M":
+	case "D": // IND, equivalent to LF
+		return ap.eventHandler.Execute(ANSI_LINE_FEED)
+	case "E": // NEL, equivalent to CRLF
+		err := ap.eventHandler.Execute(ANSI_CARRIAGE_RETURN)
+		if err == nil {
+			err = ap.eventHandler.Execute(ANSI_LINE_FEED)
+		}
+		return err
+	case "M": // RI
 		return ap.eventHandler.RI()
 	}
 
